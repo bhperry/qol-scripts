c7e2b2fa9b4c2b2e4cc1bc9e9d5583c5a95f76b3
Ben Perry <bhperry94@gmail.com>
2021-09-03 14:45:07 -0500

    aws completion

--- _source.sh
+++ _source.sh
@@ -1,5 +1,8 @@
 DIR=$( dirname "${BASH_SOURCE[0]}" )
 
+source $DIR/.env
+PYTHON_INSTALL_PATH=${PYTHON_INSTALL_PATH:-/usr/local/bin/}
+
 for FILE in $(ls $DIR | grep ".sh" | grep -v "_source\.sh"); do
     source $DIR/$FILE
 done
--- aws.sh
+++ aws.sh
@@ -1,3 +1,6 @@
+
+complete -C "${PYTHON_INSTALL_PATH}/aws_completer" aws 
+
 ecr-login() {
     REGION=${1:-us-east-2}
     ACCOUNT_ID=${2:-485185227295}
