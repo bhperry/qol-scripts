d0ee7af2fd3b405441c52b98e8fec3d3a655af27
Ben Perry <bhperry94@gmail.com>
2024-08-18 11:10:52 -0500

    Only include kubectl helpers if installed

--- kubectl.sh
+++ kubectl.sh
@@ -1,35 +1,36 @@
+if which kubectl &>/dev/null; then
+    # Kubectl
+    alias k=kubectl
+    alias kcc="kubectl config current-context"
+    alias kcn="kubectl config  get-contexts $(kcc) | tail -n +2 | sed 's/.* //'"
+    alias ktx="kubectl config use-context"
+    alias kgp="kubectl get po"
+    alias kns="kubectl config set-context --current --namespace "
+    alias kevents="kubectl get events -o custom-columns=FirstSeen:.firstTimestamp,LastSeen:.lastTimestamp,Count:.count,From:.source.component,Type:.type,Reason:.reason,Message:.message"
 
-# Kubectl
-alias k=kubectl
-alias kcc="kubectl config current-context"
-alias kcn="kubectl config  get-contexts $(kcc) | tail -n +2 | sed 's/.* //'"
-alias ktx="kubectl config use-context"
-alias kgp="kubectl get po"
-alias kns="kubectl config set-context --current --namespace "
-alias kevents="kubectl get events -o custom-columns=FirstSeen:.firstTimestamp,LastSeen:.lastTimestamp,Count:.count,From:.source.component,Type:.type,Reason:.reason,Message:.message"
+    complete -F __start_kubectl k
+    complete -F _complete_alias ktx
+    complete -F _complete_alias kgp
+    complete -F _complete_alias kns
 
-complete -F __start_kubectl k
-complete -F _complete_alias ktx
-complete -F _complete_alias kgp
-complete -F _complete_alias kns
+    klabels() {
+        local VARS="-v NAME=1"
+        local PASSTHROUGH
+        local WATCH
 
-klabels() {
-    local VARS="-v NAME=1"
-    local PASSTHROUGH
-    local WATCH
+        while [ $1 ]; do
+            case $1 in
+                --all-namespaces )
+                    VARS="-v NAME=2"
+                    PASSTHROUGH+=" $1"
+                    ;;
+                * )
+                    PASSTHROUGH+=" $1"
+                    ;;
+            esac
+            shift
+        done
 
-    while [ $1 ]; do
-        case $1 in
-            --all-namespaces )
-                VARS="-v NAME=2"
-                PASSTHROUGH+=" $1"
-                ;;
-            * )
-                PASSTHROUGH+=" $1"
-                ;;
-        esac
-        shift
-    done
-
-    kubectl get ${PASSTHROUGH:-pods} --show-labels | tail -n +2 | awk $VARS '{print $NAME"\n    "$NF"\n"}' | sed 's/,/\n    /g; s/=/: /g'
-}
+        kubectl get ${PASSTHROUGH:-pods} --show-labels | tail -n +2 | awk $VARS '{print $NAME"\n    "$NF"\n"}' | sed 's/,/\n    /g; s/=/: /g'
+    }
+fi
